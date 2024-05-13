731fffd00a06b08738a326496038d337e62a4833
shaheer311 <117433695+shaheer311@users.noreply.github.com>
2024-05-13 13:22:20 +0200

    init

---
+++ Assets/Scripts/Player script.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    [Header("Player movement")]
+    public float playerSpeed = 1.9f;
+    public float playerSprint = 3f;
+
+    [Header("Player Health Things")]
+    private float playerHealth = 120f;
+    public float presentHealth;
+    public GameObject playerDamage;
+
+    [Header("Player script cameras")]
+    public Transform playerCamera;
+
+    [Header("Player animator and gravity")]
+    public CharacterController cC;
+    public float gravity = -9.81f;
+    public Animator animator;
+    
+
+    [Header("Player jumping and velocity")]
+    public float turnCalmTime = 0.1f;
+    float turnCalmVelocity;
+    public float jumpRange = 1f;
+    Vector3 velocity;
+    public Transform surfaceCheck;
+    bool onSurface;
+    public float surfaceDistance = 0.4f;
+    public LayerMask surfaceMask;
+
+    private void Start()
+    {
+        presentHealth = playerHealth;
+    }
+
+    private void Update()
+    {
+        onSurface = Physics.CheckSphere(surfaceCheck.position, surfaceDistance, surfaceMask);
+
+        if(onSurface && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+
+        playerMove();
+
+        Jump();
+
+        Sprint();
+    }
+
+    void playerMove()
+    {
+        float horizontal_axis = Input.GetAxisRaw("Horizontal");
+        float vertical_axis = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Running", false);
+            animator.SetBool("Riflewalk", false);
+            animator.SetBool("Idleaim", false);
+
+            //rotation resten er movement
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime); 
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            cC.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Running", false);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && onSurface)
+        {
+            animator.SetBool("Idle", false);
+            animator.SetTrigger("Jump");
+
+            velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+            animator.ResetTrigger("Jump");
+        }
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButton("Sprint") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && onSurface)
+        {
+            float horizontal_axis = Input.GetAxisRaw("Horizontal");
+            float vertical_axis = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+
+                animator.SetBool("Walk", false);
+                animator.SetBool("Running", true);
+
+                //rotation resten er movement
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+                cC.Move(moveDirection.normalized * playerSprint * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Walk", true);
+                animator.SetBool("Running", false);
+            }
+        }
+    }
+    public void playerHitDamage(float takeDamage)
+    {
+        presentHealth -= takeDamage;
+        StartCoroutine(PlayerDamage());
+
+        if (presentHealth <= 0)
+        {
+            PlayerDie();
+        }
+    }
+    private void PlayerDie()
+    {
+
+        Object.Destroy(gameObject, 1.0f);
+
+    }
+
+    IEnumerator PlayerDamage()
+    {
+        playerDamage.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        playerDamage.SetActive(false);
+    }
+}
